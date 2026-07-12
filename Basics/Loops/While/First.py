77a03d7f8c2d423c64a8df55b41ce2e7e26302b0
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-07-12 19:24:52 +0000

    First program in While loop folder

---
+++ Basics/Loops/While/First.py
@@ -0,0 +1,12 @@
+"""Write a program which does the following
+Declare a variable a and initialize it to 0
+Use the syntax above to create a loop, output the following to the console
+Print the variable a in separate lines as long as it is less than 7. Don't print any other text.
+Increment a by 1 in each iteration."""
+
+
+a = 0
+
+while a < 7:
+    print(a)
+    a +=1
