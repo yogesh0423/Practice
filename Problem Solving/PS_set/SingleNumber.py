ea4b8029ceb3e1e5857478b12717a7411ead2378
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-07-28 20:44:59 +0530

    Refactor singleNumber function to remove class

--- Problem Solving/PS_set/SingleNumber.py
+++ Problem Solving/PS_set/SingleNumber.py
@@ -1,7 +1,9 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        freq = {}
+def singleNumber(nums):
+    freq = {}
 
-        for num in nums:
-            freq[num] = freq.get(num, 0) + 1
+    for num in nums:
+        freq[num] = freq.get(num, 0) + 1
 
+    for num in freq:
+        if freq[num] == 1:
+            return num
