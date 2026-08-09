a2d53b9d9bf68fa763592feb911afc2588b323f1
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-08-09 22:40:30 +0530

    Implement function to check perfect square

---
+++ Problem Solving/PS_set/ValidPerfect_square.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        low = 1
+        high = num // 2
+
+        while low <= high:
+            mid = (low + high) // 2
+            if mid * mid == num:
+                return True
+            elif mid * mid < num:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
