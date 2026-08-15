e08dadbad3011e99ef808f3d67f022e715031cb7
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-08-15 22:10:46 +0530

    Add Tuple_Basics.py with examples of tuple usage
    This file contains examples demonstrating the use of tuples in Python, including conversion to a list, accessing elements, and basic tuple operations.

---
+++ Tuples/Tuple_Basics.py
@@ -0,0 +1,26 @@
+# Program 1
+
+x = (5,6,7)
+y = list(x)
+print(y)
+print(type(y))
+y.append(20)
+print(y)
+x = tuple(y)
+print(x)
+print(type(x))
+
+
+#Program 2
+
+Fruits = ("Mango", "apple", "Grapes", "Banana")
+print(Fruits[0])
+print(Fruits[-1])
+
+
+# Program 3
+
+b = (45, 67, 454,23 )
+print(len(b))
+print(max(b))
+print(min(b))
