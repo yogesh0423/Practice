52d1c6271e9a3eba094f52c6709d8a0868810b0f
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-08-15 22:15:00 +0530

    Add examples of tuple unpacking in Python
    Add tuple unpacking examples for students and numbers.

---
+++ Tuples/Unpacking.py
@@ -0,0 +1,17 @@
+#Program 1
+
+student = ("Rahul", 21, "AIML")
+first , Middle , Last = student
+print(first)
+print(Middle)
+print(Last)
+
+
+# Program 2
+
+numbers = (10,20,30,40,50)
+first, *middle, last = numbers
+print(first)
+print(middle)
+print(last)
+
