9a8a0aff8bc9522688a47c1db100675d4331051a
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-08-16 21:54:19 +0530

    Add examples for set methods in Methods.py

--- Sets/Methods.py
+++ Sets/Methods.py
@@ -1,15 +1,29 @@
 # add()
-
 numbers = {10,20,30}
 numbers.add(40)
 print(numbers)
 
 
 # update()
-
 numbers = {10,20,30}
 numbers.update({40,50,60})
 print(numbers)
 
 
-#
+# remove()
+numbers = {10,20,30,40}
+numbers.remove(20)
+print(numbers)
+
+
+# pop()
+numbers = {10,20,30,40,50,60,70,120,100,140}
+removed = numbers.pop()
+print("Removed:", removed)
+print("Remaining:", numbers)
+
+
+# discard()
+numbers = {10,20,30,40}
+numbers.discard(50)
+print(numbers)
