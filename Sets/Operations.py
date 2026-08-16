c4a3912e7c4a362c2a59f9a9c9272c16828fdb5f
Yogesh Patil <yogeshpatil432004@gmail.com>
2026-08-16 21:56:52 +0530

    Add set operations examples in Operations.py
    Added examples for union, intersection, and difference operations on sets.

---
+++ Sets/Operations.py
@@ -0,0 +1,19 @@
+# Union
+set1 = {1,2,3}
+set2 = {3,4,5}
+print(set1 | set2)        #method1
+print(set1.union(set2))    #method2
+
+
+# intersection
+set1 = {1,2,3}
+set2 = {3,4,5,6}
+print(set1 & set2)                 # method 1
+print(set1.intersection(set2))     # method 2
+
+
+# Difference
+set1 = {1,2,3,4}
+set2 = {3,4,5}
+print(set1 - set2)               # method 1
+print(set2.difference(set1))     # method 2
